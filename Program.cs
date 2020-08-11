71976227459cf4116f90023dd1e1efea36601568
Christine Echo <christineecho@outlook.com>
2020-08-11 15:33:59 -0600

    Outlined program and added general info

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace c_grade_translator_assignment_assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            /*
+                Title: C# Grade Translator
+                Purpose: To practice comparison operators, data types, type conversion, and decisions in C#.
+                
+                Author: Krisztina Pap
+                Date of Last Edit: Aug 11, 2020
+            */
+
+            // Explain user how to use program
+
+            // Prompt user for a value
+
+            // Compare grade entered to values in table
+
+            // Output results
         }
     }
 }
