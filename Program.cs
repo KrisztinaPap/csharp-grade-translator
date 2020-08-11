7e200ca864ad80f57c77ec2d1ed44056a99b3b99
Christine Echo <christineecho@outlook.com>
2020-08-11 15:15:14 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_grade_translator_assignment_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
